21f59471bf1bc5a419b3f7b2d95752f6595910f5
pulzzar <artur_rushanian@edu.aua.am>
2021-02-24 11:41:41 +0400

    my first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def Fib():
+    f0, f1 = 0, 1
+    while True:
+        yield f0
+        f0,f1 = f1, f0 +f1
+
+fib_list = []
+
+num = int(input('INsert Your Age Here '))
+
+for a in Fib():
+    fib_list_append(a)
+    if a > num - 8:
+        Sum = sum(fib_list)
+        break
+
+print(SUm)
